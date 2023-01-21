272cb58294ca5c796c3bc68b58c72cb57b7daa56
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-01-21 18:44:17 +0530

    update nitish

--- src/contents/orders/block1/searchBar/SearchBar.jsx
+++ src/contents/orders/block1/searchBar/SearchBar.jsx
@@ -1,12 +1,17 @@
-import { Button, useTheme} from '@mui/material'
+import { Button, TextField, useTheme} from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar() {
     const theme = useTheme();
   return (
     <Box sx={{display:'flex',margin:'0 30px'}}>
-        <input style={{flex:'10',paddingLeft:'20px',borderRadius:'5px',border:`1px solid ${theme.colors.alpha.black[50]}`}} placeholder='Search your orders here' />
+        <Box sx={{flex:'10',border:`1px solid ${theme.colors.alpha.black[50]}`,}}>
+            <SearchIcon sx={{ marginTop:'6px',marginLeft:'10px', color: `${theme.colors.alpha.black[50]}`}}/>
+            <TextField sx={{paddingLeft:'10px',borderRadius:'5px',"& fieldset": { border: 'none' }, '& .MuiInputBase-input': {
+            padding: "8px"}}} placeholder='Search your orders here'></TextField>
+        </Box>
         <Button sx={{flex:'2',borderRadius:'5px'}} variant='contained'>Search</Button>
     </Box>
   )
