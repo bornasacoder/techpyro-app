1cc5d8a8d6d387bf87b833c3a986b1d941cc6030
RUPESH MANDAL <mandal707036@gamil.com>
2023-04-30 18:14:07 +0530

    rupesh

--- src/constants/carousel/carousel1.js
+++ src/constants/carousel/carousel1.js
@@ -6,7 +6,7 @@ export const mainCarousel = [
             cost: "299"
         },
         tagline: "Life's too short for boring food",
-        image:"https://images.pexels.com/photos/4620843/pexels-photo-4620843.jpeg?auto=compress&cs=tinysrgb&w=600", 
+        image:"https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&w=600", 
     },
 {
     price: {
@@ -15,7 +15,7 @@ export const mainCarousel = [
           cost: "199"
       },
       tagline: "End your hunger with us.",
-      image:"https://images.pexels.com/photos/264591/pexels-photo-264591.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/1488463/pexels-photo-1488463.jpeg?auto=compress&cs=tinysrgb&w=600",
      
 },
 {
@@ -26,7 +26,7 @@ export const mainCarousel = [
       },
 
       tagline: "Choose and taste to your place",
-      image:"https://images.pexels.com/photos/4620874/pexels-photo-4620874.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/338027/pexels-photo-338027.jpeg?auto=compress&cs=tinysrgb&w=600",
 
       
 },
@@ -38,7 +38,7 @@ export const mainCarousel = [
       },
   
       tagline: "A tasty burger is what you deserve",
-      image:"https://images.pexels.com/photos/6417971/pexels-photo-6417971.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/987209/pexels-photo-987209.jpeg?auto=compress&cs=tinysrgb&w=600",
        
 },
 {
@@ -48,7 +48,7 @@ export const mainCarousel = [
           cost: "399"
       },
       tagline: "A tasty burger is what you deserve",
-      image:"https://images.pexels.com/photos/2537930/pexels-photo-2537930.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/942320/pexels-photo-942320.jpeg?auto=compress&cs=tinysrgb&w=600",
    
 },
 
--- src/constants/fastfood.js
+++ src/constants/fastfood.js
@@ -1,7 +1,7 @@
 export const Product = [
     {
        maincategory:"fastfood",
-            shopname:"Beauty Products",
+            shopname:"Mall Products",
             id:100,
              price: {
                    mrp: "₹999",
@@ -10,14 +10,14 @@ export const Product = [
                },
                category:["Pizza" ," Pasta", " Fast Food"],
                tagline: "Pizza Theory",
-               image:"https://images.pexels.com/photos/2533266/pexels-photo-2533266.jpeg?cs=srgb&dl=pexels-suzy-hazelwood-2533266.jpg&fm=jpg",   
+               image:"https://images.pexels.com/photos/64613/pexels-photo-64613.jpeg?auto=compress&cs=tinysrgb&w=600",   
                  
                rating:"4.3",
         
    },
    {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:101,
      price: {
         mrp: "₹999",
@@ -26,7 +26,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Testy bite Pizza",
-        image:"https://thumbs.dreamstime.com/b/make-up-bag-cosmetic-beauty-products-make-up-bag-cosmetic-beauty-products-women-s-secrets-cosmetics-perfume-brushes-143303525.jpg",
+        image:"https://images.pexels.com/photos/2449665/pexels-photo-2449665.jpeg?auto=compress&cs=tinysrgb&w=600",
         image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
         image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
         image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -35,7 +35,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:102,
      price: {
         mrp: "₹999",
@@ -44,7 +44,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Love bite Pizza",
-    image:"https://www.shutterstock.com/image-photo/three-cosmetic-product-mockups-on-260nw-1970466164.jpg",
+    image:"https://images.pexels.com/photos/2733918/pexels-photo-2733918.jpeg?auto=compress&cs=tinysrgb&w=600",
     image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -53,7 +53,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:103,
      price: {
         mrp: "₹999",
@@ -62,7 +62,7 @@ export const Product = [
        },
        category:["Burger"," Pizza"," Sandwich"," Wraps", " Fast Food"," Street Food"," Beverages"],
        tagline: "Burger Monk",
-      image:"https://www.shutterstock.com/image-illustration/set-cosmetic-products-mock-white-260nw-1893565741.jpg",
+      image:"https://images.pexels.com/photos/2252584/pexels-photo-2252584.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
        image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
       image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -71,7 +71,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:104,
      price: {
         mrp: "₹999",
@@ -80,7 +80,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Pizza Hut",
-      image:"https://assets.vogue.in/photos/5d2c6732e2f0130008fa5e47/1:1/w_700,h_700,c_limit/Go%20To%20Exfoliating%20Swipeys.jpg",
+      image:"https://images.pexels.com/photos/375897/pexels-photo-375897.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -89,7 +89,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:105,
      price: {
         mrp: "₹999",
@@ -98,7 +98,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "The King of Paranthas",
-      image:"https://res.cloudinary.com/mecca/image/upload/t_tablet/v1597631243/brand-pages/Go-To/go-to-skincare-review-landscape_w0ftbz.jpg",
+      image:"https://images.pexels.com/photos/3962292/pexels-photo-3962292.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
      image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
      image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -108,7 +108,7 @@ export const Product = [
 
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:110,
      price: {
         mrp: "₹999",
@@ -117,7 +117,7 @@ export const Product = [
        },
        category:[" Finger Chips"," Samosa"," Spring Rolls"],
        tagline: "Hukum Pizza",
-       image:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1659540630-trademark-beauty-the-essentials-makeup-brush-collection-by-tobi-henney-1659540620.jpg?crop=1.00xw:0.801xh;0,0&resize=980:*",
+       image:"https://images.pexels.com/photos/1187299/pexels-photo-1187299.jpeg?auto=compress&cs=tinysrgb&w=600",
        image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -126,7 +126,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:111,
      price: {
         mrp: "₹999",
@@ -135,7 +135,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Piper Pizza",  
-        image:"https://img.buzzfeed.com/buzzfeed-static/static/2019-12/19/19/asset/23e2ac8825ec/sub-buzz-1274-1576784613-2.png",
+        image:"https://images.pexels.com/photos/3962286/pexels-photo-3962286.jpeg?auto=compress&cs=tinysrgb&w=600",
         image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -144,7 +144,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Beauty Products",
+   shopname:"Mall Products",
     id:112,
      price: {
         mrp: "₹999",
@@ -153,7 +153,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Mexican Pizza",
-      image:"https://rukminim1.flixcart.com/image/312/312/kd3f3bk0/brush-applicator/4/9/5/professional-black-handle-single-makeup-brush-with-oval-makeup-original-imafu2s3hgxgsyfz.jpeg?q=70",
+      image:"https://images.pexels.com/photos/9811233/pexels-photo-9811233.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
       image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
       image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -162,7 +162,7 @@ export const Product = [
 },
 {
     maincategory:"pizza",
-    shopname:"Beauty Products",
+    shopname:"Mall Products",
     id:113,
     price: {
         mrp: "₹999",
@@ -171,7 +171,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB2476h6fZQbQJO4LKSrcMLMUvvhHnDUmnkA&usqp=CAU",
+      image: "https://images.pexels.com/photos/3962287/pexels-photo-3962287.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1: "https://b.zmtcdn.com/data/pictures/9/19857789/dbe75c6d7ffda09b57ae01eed48deedf.jpg?fit=around|300:273&crop=300:273;*,*",
       image2: "https://b.zmtcdn.com/data/pictures/chains/4/19183764/eb85111153543e0d8e9f572914326738.jpg?fit=around|300:273&crop=300:273;*,* ",
       image3: "https://b.zmtcdn.com/data/pictures/9/19857789/99611fcc97908f4abd29d75590529640.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -179,7 +179,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Beauty Products",
+    shopname:"Mall Products",
     id:114,
     price: {
         mrp: "₹999",
@@ -188,7 +188,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-      image: "https://images.unsplash.com/photo-1629198688000-71f23e745b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVhdXR5JTIwcHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80",
+      image: "https://images.pexels.com/photos/7679863/pexels-photo-7679863.jpeg?auto=compress&cs=tinysrgb&w=600",
       image1: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/ca1f4478ebd73e06018d8dfd7ad3f06c.jpg?fit=around|300:273&crop=300:273;*,*",
       image2: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/ca71b77e366f50dbd45501668de1bf5d.jpg?fit=around|300:273&crop=300:273;*,*",
       image3: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/e0e6212c11a6b573e7b3ad4edfef97d5.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -196,14 +196,14 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Beauty Products",
+    shopname:"Mall Products",
     id:115,
     price: {
         mrp: "₹999",
         discount: "FLAT 25% OFF",
           cost: "₹499"
       },
-     image: "hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8TTja2fIakpdFwnAuvSoh3xYusywf4BoeIQ&usqp=CAU",
+     image: "https://images.pexels.com/photos/16170/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600",
      image1: "https://b.zmtcdn.com/data/pictures/chains/7/19269047/a6d7a5fc0061ba9bc8209fb25b52a98c.jpg?fit=around|300:273&crop=300:273;*,* ",
      image2: "https://b.zmtcdn.com/data/pictures/7/19269047/f5edea330bb55864c9b8bd7292f26862.jpg?fit=around|300:273&crop=300:273;*,* ",
      image3: "https://b.zmtcdn.com/data/reviews_photos/6f8/a96801780ae7e0877a48aad5a7bcb6f8_1651066005.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -213,7 +213,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Beauty Products",
+    shopname:"Mall Products",
     id:116,
     price: {
         mrp: "₹999",
@@ -222,7 +222,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-    image: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Cosmetics.JPG ",
+    image: "https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600",
     image1: "https://b.zmtcdn.com/data/pictures/chains/0/19553770/731a339f387a7c2e4ca59da3121cbec3.jpg?fit=around|300:273&crop=300:273;*,* ",
     image2: "https://b.zmtcdn.com/data/pictures/chains/0/19553770/fd0767fbf161bd531a7e3b319274f581.jpg?fit=around|300:273&crop=300:273;*,*",
     image3: "https://b.zmtcdn.com/data/reviews_photos/3e0/e9602b9b8a32d12f2f58f66fa1b2d3e0_1622786891.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -230,7 +230,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Beauty Products",
+    shopname:"Mall Products",
     id:117,
     price: {
         mrp: "₹999",
@@ -239,7 +239,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrK11pTLznvrjANSJzlbdy3rlTmIN912kF8E2yNvMQ_58yNk8KC1JM2T9dx51QdKFDw6I&usqp=CAU ",
+    image:"https://images.pexels.com/photos/6069977/pexels-photo-6069977.jpeg?auto=compress&cs=tinysrgb&w=600 ",
     image1:"https://b.zmtcdn.com/data/reviews_photos/9f5/3f3d292ad24ae2773097d56a490dd9f5_1639725443.jpg?fit=around|300:273&crop=300:273;*,*",
     image2:"https://b.zmtcdn.com/data/reviews_photos/868/9624d8cd2cfcafe23ed409ef98bd1868_1639725442.jpg?fit=around|300:273&crop=300:273;*,*",
     image3:"https://b.zmtcdn.com/data/reviews_photos/683/8c0f94f169f50da0712cf99bb39f9683_1643131025.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -249,7 +249,7 @@ export const Product = [
 
        {
         maincategory:"other",
-        shopname:"Beauty Products",
+        shopname:"Mall Products",
         id:131,
          price: {
             mrp: "₹999",
@@ -257,14 +257,14 @@ export const Product = [
                cost: "₹499"
            },
            category:["Spring Rolls","Thali","sweets"],
-           image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdgJVlsBrDgUeXa1Z3suxQUCAz1kEeonR2Uw&usqp=CAU",
+           image:"https://images.pexels.com/photos/3875430/pexels-photo-3875430.jpeg?auto=compress&cs=tinysrgb&w=600",
            
            tagline: "Life's too short for boring food",
            rating:"4.3",
        },
        {
         maincategory:"pizza",
-        shopname:"Beauty Products",
+        shopname:"Mall Products",
         id:113,
         price: {
             mrp: "₹999",
@@ -273,7 +273,7 @@ export const Product = [
           },
           category:["Pizza","California Pizza","Detroit Pizza"],
           tagline: "Life's too short for boring food",
-          image: "https://images.unsplash.com/photo-1596462502278-27bfdc403348?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVhdXR5JTIwcHJvZHVjdHN8ZW58MHx8MHx8&w=1000&q=80",
+          image: "https://images.pexels.com/photos/8386666/pexels-photo-8386666.jpeg?auto=compress&cs=tinysrgb&w=600",
           image1: "https://b.zmtcdn.com/data/pictures/9/19857789/dbe75c6d7ffda09b57ae01eed48deedf.jpg?fit=around|300:273&crop=300:273;*,*",
           image2: "https://b.zmtcdn.com/data/pictures/chains/4/19183764/eb85111153543e0d8e9f572914326738.jpg?fit=around|300:273&crop=300:273;*,* ",
           image3: "https://b.zmtcdn.com/data/pictures/9/19857789/99611fcc97908f4abd29d75590529640.jpg?fit=around|300:273&crop=300:273;*,*",
