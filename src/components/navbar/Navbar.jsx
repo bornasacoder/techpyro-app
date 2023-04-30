4353a9c9499b7f84fb256dfa0e25ddf9f1866c03
RUPESH MANDAL <mandal707036@gamil.com>
2023-04-30 09:03:03 +0530

    rupesh

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -263,12 +263,12 @@ export default function Navbar() {
         <Box sx={{width:{xs:'300px',sm:'500px'},height:{xs:'120px',sm:'180px'},border:'1px solid rgba(0,0,0,0.2)',borderRadius:'20px'}}>
             <Box sx={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
                 <Box sx={{display:'flex',flexDirection:'column',margin:'10px',justifyContent:'center'}}>
-                    <Typography sx={{fontSize:'30px',fontWeight:'700'}}>Restaurents</Typography>
-                    <Typography>Enjoy your favourite treats</Typography>
+                    <Typography sx={{fontSize:'30px',fontWeight:'700'}}>Groceries</Typography>
+                    <Typography>Enjoy your favourite groceries</Typography>
                     <Typography sx={{fontSize:'17px',fontWeight:'600'}}>View All</Typography>
                 </Box>
                 <Box sx={{margin:'10px'}}>
-                   <img src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/pneknawbadtvceqzwiep' style={{height:'100px',width:'100px',borderRadius:'10px'}}/>
+                   <img src='https://images.pexels.com/photos/1666067/pexels-photo-1666067.jpeg?auto=compress&cs=tinysrgb&w=600' style={{height:'100px',width:'100px',borderRadius:'10px'}}/>
                 </Box>
             </Box>
 
