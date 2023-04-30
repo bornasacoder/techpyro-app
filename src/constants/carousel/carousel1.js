8c03a6dfcb67be81cca459f1cf316dd7ee1db85d
RUPESH MANDAL <mandal707036@gamil.com>
2023-04-30 17:23:18 +0530

    rupesh

--- src/constants/carousel/carousel1.js
+++ src/constants/carousel/carousel1.js
@@ -6,7 +6,7 @@ export const mainCarousel = [
             cost: "299"
         },
         tagline: "Life's too short for boring food",
-        image:"https://images.pexels.com/photos/1571463/pexels-photo-1571463.jpeg?auto=compress&cs=tinysrgb&w=600", 
+        image:"https://images.pexels.com/photos/208512/pexels-photo-208512.jpeg?auto=compress&cs=tinysrgb&w=600", 
     },
 {
     price: {
@@ -15,7 +15,7 @@ export const mainCarousel = [
           cost: "199"
       },
       tagline: "End your hunger with us.",
-      image:"https://images.pexels.com/photos/276566/pexels-photo-276566.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/4046997/pexels-photo-4046997.jpeg?auto=compress&cs=tinysrgb&w=600",
      
 },
 {
@@ -26,7 +26,7 @@ export const mainCarousel = [
       },
 
       tagline: "Choose and taste to your place",
-      image:"https://images.pexels.com/photos/1579739/pexels-photo-1579739.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.pexels.com/photos/14797855/pexels-photo-14797855.jpeg?auto=compress&cs=tinysrgb&w=600",
 
       
 },
@@ -38,7 +38,7 @@ export const mainCarousel = [
       },
   
       tagline: "A tasty burger is what you deserve",
-      image:"https://images.pexels.com/photos/1648768/pexels-photo-1648768.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://img.lovepik.com/photo/20211208/small/lovepik-medicine-pills-cold-medicine-anti-inflammatory-picture_501580402.jpg",
        
 },
 {
@@ -48,7 +48,7 @@ export const mainCarousel = [
           cost: "399"
       },
       tagline: "A tasty burger is what you deserve",
-      image:"https://images.pexels.com/photos/1329711/pexels-photo-1329711.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://img.lovepik.com/photo/20211122/small/lovepik-medicine-in-medicine-picture_500739404.jpg",
    
 },
 
--- src/constants/fastfood.js
+++ src/constants/fastfood.js
@@ -1,7 +1,7 @@
 export const Product = [
     {
        maincategory:"fastfood",
-            shopname:"Furnitures",
+            shopname:"Medicals",
             id:100,
              price: {
                    mrp: "₹999",
@@ -10,14 +10,14 @@ export const Product = [
                },
                category:["Pizza" ," Pasta", " Fast Food"],
                tagline: "Pizza Theory",
-               image:"https://images.pexels.com/photos/1571471/pexels-photo-1571471.jpeg?auto=compress&cs=tinysrgb&w=600",   
+               image:"https://5.imimg.com/data5/SELLER/Default/2022/12/NZ/PQ/BW/34192431/pharmaceutical-tablets-500x500.jpeg",   
                  
                rating:"4.3",
         
    },
    {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:101,
      price: {
         mrp: "₹999",
@@ -26,7 +26,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Testy bite Pizza",
-        image:"https://images.pexels.com/photos/2343465/pexels-photo-2343465.jpeg?auto=compress&cs=tinysrgb&w=600",
+        image:"https://5.imimg.com/data5/WF/NP/MY-2755389/cefixime-ofloxacin-500x500.jpg",
         image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
         image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
         image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -35,7 +35,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:102,
      price: {
         mrp: "₹999",
@@ -44,7 +44,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Love bite Pizza",
-    image:"https://images.pexels.com/photos/1571452/pexels-photo-1571452.jpeg?auto=compress&cs=tinysrgb&w=600",
+    image:"https://5.imimg.com/data5/SELLER/Default/2021/8/QH/TK/DF/10750219/cefixime-and-ofloxacin-tablets-500x500.jpeg",
     image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -53,7 +53,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:103,
      price: {
         mrp: "₹999",
@@ -62,7 +62,7 @@ export const Product = [
        },
        category:["Burger"," Pizza"," Sandwich"," Wraps", " Fast Food"," Street Food"," Beverages"],
        tagline: "Burger Monk",
-      image:"https://images.pexels.com/photos/1571467/pexels-photo-1571467.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://5.imimg.com/data5/SELLER/Default/2023/1/KV/YJ/CW/9915129/hefi-plus-cefixime-ofloxacin-tablets-250x250.jpeg",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
        image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
       image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -71,7 +71,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:104,
      price: {
         mrp: "₹999",
@@ -80,7 +80,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Pizza Hut",
-      image:"https://images.pexels.com/photos/1648838/pexels-photo-1648838.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVkaWNpbmV8ZW58MHx8MHx8&w=1000&q=80",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -89,7 +89,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:105,
      price: {
         mrp: "₹999",
@@ -98,7 +98,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "The King of Paranthas",
-      image:"https://images.pexels.com/photos/7055757/pexels-photo-7055757.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://i.guim.co.uk/img/media/f7ab20f9e5f139ab5a7c6220993b31e089ac1162/0_180_5500_3299/master/5500.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=75c989e67d7cf42c714f8d8cfb4a803a",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
      image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
      image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -108,7 +108,7 @@ export const Product = [
 
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:110,
      price: {
         mrp: "₹999",
@@ -117,7 +117,7 @@ export const Product = [
        },
        category:[" Finger Chips"," Samosa"," Spring Rolls"],
        tagline: "Hukum Pizza",
-       image:"https://images.pexels.com/photos/3773572/pexels-photo-3773572.png?auto=compress&cs=tinysrgb&w=600",
+       image:"https://d2cbg94ubxgsnp.cloudfront.net/Pictures/1024x536/3/1/4/138314_productshot_large_6.25.18.jpg",
        image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -126,7 +126,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:111,
      price: {
         mrp: "₹999",
@@ -135,7 +135,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Piper Pizza",  
-        image:"https://images.pexels.com/photos/1648839/pexels-photo-1648839.jpeg?auto=compress&cs=tinysrgb&w=600",
+        image:"https://g.foolcdn.com/editorial/images/548920/5916202_givlaari_cartonvial_angled1.jpg",
         image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
                image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
                image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -144,7 +144,7 @@ export const Product = [
 },
 {
    maincategory:"fastfood",
-   shopname:"Furnitures",
+   shopname:"Medicals",
     id:112,
      price: {
         mrp: "₹999",
@@ -153,7 +153,7 @@ export const Product = [
        },
        category:["Finger Chips","Samosa","Spring Rolls"],
        tagline: "Da Mexican Pizza",
-      image:"https://images.pexels.com/photos/1090092/pexels-photo-1090092.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8sBIjD5LuLLGV9v8zfe4ZaF6x5SEbZjZuu-2ew_TIgRmj5OEl5-rgkxDmGNIDSpYFrNM&usqp=CAU",
       image1:"https://b.zmtcdn.com/data/pictures/5/18285165/0fc8a333af04eb4fa92a59425972fed7.jpg?fit=around|300:273&crop=300:273;*,*",   
       image2:"https://b.zmtcdn.com/data/pictures/5/18285165/fe2cd6ca3f27e4a473241bcd9fb33a29.jpg?fit=around|300:273&crop=300:273;*,*",   
       image3:"https://b.zmtcdn.com/data/reviews_photos/975/334020e174222d65ee742abf75cce975_1567934297.jpg?fit=around|300:273&crop=300:273;*,*", 
@@ -162,7 +162,7 @@ export const Product = [
 },
 {
     maincategory:"pizza",
-    shopname:"Furnitures",
+    shopname:"Medicals",
     id:113,
     price: {
         mrp: "₹999",
@@ -171,7 +171,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-      image: "https://images.pexels.com/photos/3773574/pexels-photo-3773574.png?auto=compress&cs=tinysrgb&w=600 ",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxaYm5H-rtJYdBqP8R85eO8ziD6NakCaKI9w&usqp=CAU ",
       image1: "https://b.zmtcdn.com/data/pictures/9/19857789/dbe75c6d7ffda09b57ae01eed48deedf.jpg?fit=around|300:273&crop=300:273;*,*",
       image2: "https://b.zmtcdn.com/data/pictures/chains/4/19183764/eb85111153543e0d8e9f572914326738.jpg?fit=around|300:273&crop=300:273;*,* ",
       image3: "https://b.zmtcdn.com/data/pictures/9/19857789/99611fcc97908f4abd29d75590529640.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -179,7 +179,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Furnitures",
+    shopname:"Medicals",
     id:114,
     price: {
         mrp: "₹999",
@@ -188,7 +188,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-      image: "https://images.pexels.com/photos/11011890/pexels-photo-11011890.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaeOrosahT0sXQ1C7BQpy2EitruhNFZ0OUXA&usqp=CAU",
       image1: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/ca1f4478ebd73e06018d8dfd7ad3f06c.jpg?fit=around|300:273&crop=300:273;*,*",
       image2: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/ca71b77e366f50dbd45501668de1bf5d.jpg?fit=around|300:273&crop=300:273;*,*",
       image3: "https://b.zmtcdn.com/data/pictures/chains/1/19461231/e0e6212c11a6b573e7b3ad4edfef97d5.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -196,14 +196,14 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Furnitures",
+    shopname:"Medicals",
     id:115,
     price: {
         mrp: "₹999",
         discount: "FLAT 25% OFF",
           cost: "₹499"
       },
-     image: "https://images.pexels.com/photos/1743226/pexels-photo-1743226.jpeg?auto=compress&cs=tinysrgb&w=600",
+     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_vngByK7kUxTTtxyrlB0_UqAvCYqyYm2IUDL32ARwEqq-YhWfI3_jyFs0pjyS-ZW5Q4c&usqp=CAU",
      image1: "https://b.zmtcdn.com/data/pictures/chains/7/19269047/a6d7a5fc0061ba9bc8209fb25b52a98c.jpg?fit=around|300:273&crop=300:273;*,* ",
      image2: "https://b.zmtcdn.com/data/pictures/7/19269047/f5edea330bb55864c9b8bd7292f26862.jpg?fit=around|300:273&crop=300:273;*,* ",
      image3: "https://b.zmtcdn.com/data/reviews_photos/6f8/a96801780ae7e0877a48aad5a7bcb6f8_1651066005.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -213,7 +213,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Furnitures",
+    shopname:"Medicals",
     id:116,
     price: {
         mrp: "₹999",
@@ -222,7 +222,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-    image: "https://images.pexels.com/photos/2725675/pexels-photo-2725675.jpeg?auto=compress&cs=tinysrgb&w=600 ",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU_UJFKVcJMa55RNK5FK3IJsD4NF2DRk9Yvw&usqp=CAU ",
     image1: "https://b.zmtcdn.com/data/pictures/chains/0/19553770/731a339f387a7c2e4ca59da3121cbec3.jpg?fit=around|300:273&crop=300:273;*,* ",
     image2: "https://b.zmtcdn.com/data/pictures/chains/0/19553770/fd0767fbf161bd531a7e3b319274f581.jpg?fit=around|300:273&crop=300:273;*,*",
     image3: "https://b.zmtcdn.com/data/reviews_photos/3e0/e9602b9b8a32d12f2f58f66fa1b2d3e0_1622786891.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -230,7 +230,7 @@ export const Product = [
   },
   {
     maincategory:"pizza",
-    shopname:"Furnitures",
+    shopname:"Medicals",
     id:117,
     price: {
         mrp: "₹999",
@@ -239,7 +239,7 @@ export const Product = [
       },
       category:["Pizza","California Pizza","Detroit Pizza"],
       tagline: "Life's too short for boring food",
-    image:"https://images.pexels.com/photos/1181394/pexels-photo-1181394.jpeg?auto=compress&cs=tinysrgb&w=600 ",
+    image:"https://www.herbaldaily.in/Uploads/Products/b605537c-f143-4d15-8870-992a61a20b8a/68272e9d-d6a4-4c82-9c89-05653479b133/_large.jpg ",
     image1:"https://b.zmtcdn.com/data/reviews_photos/9f5/3f3d292ad24ae2773097d56a490dd9f5_1639725443.jpg?fit=around|300:273&crop=300:273;*,*",
     image2:"https://b.zmtcdn.com/data/reviews_photos/868/9624d8cd2cfcafe23ed409ef98bd1868_1639725442.jpg?fit=around|300:273&crop=300:273;*,*",
     image3:"https://b.zmtcdn.com/data/reviews_photos/683/8c0f94f169f50da0712cf99bb39f9683_1643131025.jpg?fit=around|300:273&crop=300:273;*,*",
@@ -249,7 +249,7 @@ export const Product = [
 
        {
         maincategory:"other",
-        shopname:"Furnitures",
+        shopname:"Medicals",
         id:131,
          price: {
             mrp: "₹999",
@@ -257,14 +257,14 @@ export const Product = [
                cost: "₹499"
            },
            category:["Spring Rolls","Thali","sweets"],
-           image:"https://images.pexels.com/photos/3285193/pexels-photo-3285193.jpeg?auto=compress&cs=tinysrgb&w=600",
+           image:"https://img.washingtonpost.com/rf/image_480w/2010-2019/WashingtonPost/2013/12/06/National-Economy/Images/LUCENTIS%20Product%20Shot1386367811.jpg?uuid=LxN2kF7DEeONJDHAFrl2sg",
            
            tagline: "Life's too short for boring food",
            rating:"4.3",
        },
        {
         maincategory:"pizza",
-        shopname:"Furnitures",
+        shopname:"Medicals",
         id:113,
         price: {
             mrp: "₹999",
@@ -273,7 +273,7 @@ export const Product = [
           },
           category:["Pizza","California Pizza","Detroit Pizza"],
           tagline: "Life's too short for boring food",
-          image: "https://images.pexels.com/photos/7546715/pexels-photo-7546715.jpeg?auto=compress&cs=tinysrgb&w=600",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFjYSGBf9cenlfi2WIZODBGQWspCzcjnxHFw&usqp=CAU",
           image1: "https://b.zmtcdn.com/data/pictures/9/19857789/dbe75c6d7ffda09b57ae01eed48deedf.jpg?fit=around|300:273&crop=300:273;*,*",
           image2: "https://b.zmtcdn.com/data/pictures/chains/4/19183764/eb85111153543e0d8e9f572914326738.jpg?fit=around|300:273&crop=300:273;*,* ",
           image3: "https://b.zmtcdn.com/data/pictures/9/19857789/99611fcc97908f4abd29d75590529640.jpg?fit=around|300:273&crop=300:273;*,*",
