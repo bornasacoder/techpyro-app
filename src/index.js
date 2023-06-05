1b6ca37823830aeab3fe07faf5ca3891d4a8a0c7
Ashish Singh <88818431+bornasacoder@users.noreply.github.com>
2023-06-05 19:06:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App'; 
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import {PersistGate} from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
