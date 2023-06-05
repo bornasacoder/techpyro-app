da2f9c8a89f6937cacd08f81edc6e5ad73d86b6d
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-05 18:59:42 +0530

    rupesh

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import ThemeProviderWrapper from 'theme/ThemeProvider';
+import App from './App'; 
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import {PersistGate} from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProviderWrapper>
     <Provider store = {store}>
       <PersistGate loading={null} persistor={persistor} >
     <CssBaseline/>
     <App />
       </PersistGate>
     </Provider>
-  </ThemeProviderWrapper>
 );
 
