b6de973633f831d5bf8d414f2d1c2e3a5c302987
= <=>
2023-06-06 16:40:00 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-
-
 import Navbar from "../../components/navbar/Navbar";
-
 import Footer from '../../components/footer/Footer'
 import Block1 from '../../contents/home/Block1'
-import Block2 from 'contents/Block2';
-
+import Block2 from '../../contents/Block2';
 import Block4 from '../../contents/home/Block4'
-
 import SwiggySlider from '../../components/allcarousel/carousel/carousel2/SwiggySlider';
 import BigSlider from '../../components/allcarousel/slider/slider3/BigSlider';
 import { other } from '../../constants/slider/other';
@@ -17,8 +12,6 @@ import { adidas } from '../../constants/slider/adidas'
 import MiniSlider from '../../components/allcarousel/slider/slider2/MiniSlider';
 
 import { pizza } from '../../constants/slider/pizza';
-
-
 import Block8 from '../../contents/home/Block8';
 
 const Home = () => {
