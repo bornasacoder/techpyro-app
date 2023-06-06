1d105fb0b3558d668fbbed3ca08a320def1558d2
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-06 16:01:56 +0530

    rupesh

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import SearchBar from '../searchbar/SearchBar'
-import NavButton from 'components/navButton/NavButton'
+import NavButton from '../../components/navButton/NavButton'
 import {
     AppBar,
     Box,
