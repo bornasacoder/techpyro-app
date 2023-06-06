f4cb074553c530e1ccb65bf859697129929fd7a1
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-06 17:06:21 +0530

    rupesh

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import ThemeProviderWrapper from './theme/ThemeProvider';
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import {PersistGate} from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
