e5a3fcc55777518f16c84a79904adf3835e57195
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-06 16:26:25 +0530

    rupesh

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Card2 from '../../components/card/card2/Card2'
 import Footer from '../../components/footer/Footer'
 import Block1 from '../../contents/home/Block1'
-import Block2 from 'contents/Block2';
+import Block2 from '../../contents/Block2';
 import Block3 from '../../contents/home/Block3'
 import Block4 from '../../contents/home/Block4'
 import { content } from '../../constants/content';
