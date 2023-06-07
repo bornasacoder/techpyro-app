d5201ea4e50de307e20db6546cbd7bfa5f9da581
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-07 20:22:46 +0530

    updatee

--- src/components/pages/Offers.jsx
+++ src/components/pages/Offers.jsx
@@ -1,9 +1,9 @@
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import Card1 from 'components/card/Card1'
-import Card2 from 'components/card/Card2'
-import Footer from 'components/footer/Footer'
-import Navbar from 'components/navbar/Navbar'
+import Card1 from '../../components/Card/Card1'
+import Card2 from '../../components/Card/Card2'
+import Footer from '../../components/footer/Footer'
+import Navbar from '../../components/navbar/Navbar'
 import React from 'react'
 
 const Offers = () => {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ThemeProviderWrapper from 'theme/ThemeProvider';
+import ThemeProviderWrapper from './theme/ThemeProvider';
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import {PersistGate} from "redux-persist/integration/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
