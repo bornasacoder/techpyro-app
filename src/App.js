cd500b72eb30d6a454dda03e33fdf4c67f73fe83
= <=>
2023-06-07 20:11:36 +0530

    comiit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Home from "pages/home/Home";
-import Login from "pages/login/Login";
-import Signup from "pages/signup/Signup";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 import {
   BrowserRouter as Router,
   Routes,
