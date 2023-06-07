c78c1596297bf52b82599d40310f17f176780aa9
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-06-07 17:25:52 +0530

    nitish

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import SearchBar from "components/navbar/searchbar/SearchBar";
-import NavButton from "components/navbar/navButton/NavButton";
+import SearchBar from "../../components/navbar/searchbar/SearchBar";
+import NavButton from ".././components/navbar/navButton/NavButton";
 // import NavImage from 'images/category/png-10.jpeg'
 import {
   AppBar,
@@ -49,8 +49,8 @@ import {
 } from "@mui/icons-material";
 import BreakfastDiningIcon from '@mui/icons-material/BreakfastDining';
 // import MenuButtons from "./menuButtons/MenuButtons";
-import MainCarousel from "components/carousel/MainCarousel";
-import Content from "components/content/Content";
+import MainCarousel from "../../components/carousel/MainCarousel";
+import Content from "../../components/content/Content";
 import MenuButtons from "./menuButtons/MenuButtons";
 import StorefrontIcon from '@mui/icons-material/Storefront';
 
