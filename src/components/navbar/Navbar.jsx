06e9a506b1b3a8042342d41c68df4766c046c3a9
= <=>
2023-06-07 17:43:57 +0530

    fhhf

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
-import SearchBar from 'components/navbar/searchbar/SearchBar'
-import NavButton from 'components/navButton/NavButton'
+import SearchBar from '../navbar/searchbar/SearchBar'
+import NavButton from '../navButton/NavButton'
 import {
     AppBar,
     Box,
@@ -20,10 +20,10 @@ import {
 
 import { Help, Home, Menu, PercentRounded, PercentTwoTone, Person, Person2Outlined, Search, ShoppingBag, ShoppingCart} from '@mui/icons-material';
 
-import MenuButtons from 'components/menuButtons/MenuButtons';
+import MenuButtons from '../menuButtons/MenuButtons';
 import { Link } from 'react-router-dom';
-import Login from 'components/pages/Login';
-import { getCordinate } from 'components/Location/Location';
+import Login from '../pages/Login';
+import { getCordinate } from '../Location/Location';
 import { useEffect } from 'react';
 
 const StyleToolbar = styled(Toolbar)(({theme}) => ({
