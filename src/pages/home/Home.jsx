9469399910904e9f9b8a0fb59e8ea0238e3a45d5
= <=>
2023-06-07 21:03:15 +0530

    comiit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import Footer from '../../components/footer/Footer'
 import { other } from 'constants/slider/other';
 import { adidas } from '../../constants/slider/adidas'
-import { pizza } from 'constants/slider/pizza';
-import Block2 from 'contents/home/Block2/Block2';
-import Block5 from 'contents/home/Block5/Block5';
-import Block1 from 'contents/home/Block1/Block1';
-import Block3 from 'contents/home/Block3/Block3';
-import Block4 from 'contents/home/Block4/block4';
-import Navbar from 'layouts/Navbar/Navbar';
+import { pizza } from '../../constants/slider/pizza';
+import Block2 from '../../contents/home/Block2/Block2';
+import Block5 from '../../contents/home/Block5/Block5';
+import Block1 from '../../contents/home/Block1/Block1';
+import Block3 from '../../contents/home/Block3/Block3';
+import Block4 from '../../contents/home/Block4/block4';
+import Navbar from '../../layouts/Navbar/Navbar';
 
 const Home = () => {
 // console.log(mainCarousel);
