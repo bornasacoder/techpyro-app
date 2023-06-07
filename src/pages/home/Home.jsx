7a6bb944e990e268c2cc4741d0d8a2bc95597d03
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-06-07 18:11:51 +0530

    nitish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,29 @@
-// import MainCarousel from 'components/carousel/MainCarousel'
-import Sliders from 'components/slider/slider1/Sliders'
+
+import Sliders from '../../components/slider/slider1/Sliders'
 import React from 'react'
-import MiniSlider from 'components/slider/slider2/MiniSlider';
+import MiniSlider from '../../components/slider/slider2/MiniSlider';
 import {shoes} from "../../constants/slider/shoes";
 import {thali} from "../../constants/slider/thali";
 import {fastFood} from "../../constants/slider/jogger";
 import {other} from "../../constants/slider/other";
-import Navbar from 'components/navbar/Navbar';
-import MiddleContent from 'components/middlecontent/MiddleContent';
-import Footer from 'components/footer/Footer';
-import Content from 'components/content/Content';
-import Navbar2 from 'components/navbar/Navbar2';
-import Content1 from 'components/content/Content1';
-import Content2 from 'components/content/Content2';
-import Content3 from 'components/content/Content3';
-import Content4 from 'components/content/Content4';
-import Content5 from 'components/content/Content5';
-import Content6 from 'components/content/Content6';
-import Content7 from 'components/content/Content7';
-import Block1 from 'contents/home/block1/Block1';
-import Block2 from 'contents/home/block2/Block2';
-import Block3 from 'contents/home/block3/Block3';
-import Content8 from 'components/content/Content8';
-import Content9 from 'components/content/Content9';
-import Content10 from 'components/content/Content10';
+import Navbar from '../../components/navbar/Navbar';
+import MiddleContent from '../../components/middlecontent/MiddleContent';
+import Footer from '../../components/footer/Footer';
+import Content from '../../components/content/Content';
+import Navbar2 from '../../components/navbar/Navbar2';
+import Content1 from '../../components/content/Content1';
+import Content2 from '../../components/content/Content2';
+import Content3 from '../../components/content/Content3';
+import Content4 from '../../components/content/Content4';
+import Content5 from '../../components/content/Content5';
+import Content6 from '../../components/content/Content6';
+import Content7 from '../../components/content/Content7';
+import Block1 from '../../contents/home/block1/Block1';
+import Block2 from '../../contents/home/block2/Block2';
+import Block3 from '../../contents/home/block3/Block3';
+import Content8 from '../../components/content/Content8';
+import Content9 from '../../components/content/Content9';
+import Content10 from '../../components/content/Content10';
 
 
 const Home = () => {
