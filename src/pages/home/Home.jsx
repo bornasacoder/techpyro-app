4002f3b75f14006b7043374ce67667d0edad0f16
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-07 19:20:36 +0530

    updatee

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,18 +5,18 @@ import Navbar from "../../components/navbar/Navbar";
 import Card2 from '../../components/card/card2/Card2'
 import Footer from '../../components/footer/Footer'
 import Block1 from '../../contents/home/Block1'
-import Block2 from 'contents/Block2';
+import Block2 from '../../contents/Block2';
 import Block3 from '../../contents/home/Block3'
 import Block4 from '../../contents/home/Block4'
-import { content } from 'constants/content';
-import Block5 from 'contents/home/Block5';
-import SwiggySlider from 'components/allcarousel/carousel/carousel2/SwiggySlider';
-import BigSlider from 'components/allcarousel/slider/slider3/BigSlider';
-import { other } from 'constants/slider/other';
+import { content } from '../../constants/content';
+import Block5 from '../../contents/home/Block5';
+import SwiggySlider from '../../components/allcarousel/carousel/carousel2/SwiggySlider';
+import BigSlider from '../../components/allcarousel/slider/slider3/BigSlider';
+import { other } from '../../constants/slider/other';
 import { adidas } from '../../constants/slider/adidas'
-import Block6 from 'contents/home/Block6';
-import MiniSlider from 'components/allcarousel/slider/slider2/MiniSlider';
-import { pizza } from 'constants/slider/pizza';
+import Block6 from '../../contents/home/Block6';
+import MiniSlider from '../../components/allcarousel/slider/slider2/MiniSlider';
+import { pizza } from '../../constants/slider/pizza';
 
 const Home = () => {
 // console.log(mainCarousel);
