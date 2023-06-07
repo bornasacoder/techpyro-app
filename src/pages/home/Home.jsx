7136bc21cd49aa113d6c712e48aa9e7cbdd22d26
RUPESH MANDAL <mandal707036@gamil.com>
2023-06-07 20:30:19 +0530

    updatee

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,14 +5,14 @@ import Navbar from "../../components/navbar/Navbar";
 import Card2 from '../../components/card/card2/Card2'
 import Footer from '../../components/footer/Footer'
 import Block1 from '../../contents/home/Block1'
-import Block2 from 'contents/Block2';
+import Block2 from '../../contents/Block2';
 import Block3 from '../../contents/home/Block3'
 import Block4 from '../../contents/home/Block4'
-import { content } from 'constants/content';
-import Block5 from 'contents/home/Block5';
-import SwiggySlider from 'components/allcarousel/carousel/carousel2/SwiggySlider';
-import BigSlider from 'components/allcarousel/slider/slider3/BigSlider';
-import { other } from 'constants/slider/other';
+import { content } from '../../constants/content';
+import Block5 from '../../contents/home/Block5';
+import SwiggySlider from '../../components/allcarousel/carousel/carousel2/SwiggySlider';
+import BigSlider from '../../components/allcarousel/slider/slider3/BigSlider';
+import { other } from '../../constants/slider/other';
 import { adidas } from '../../constants/slider/adidas'
 const Home = () => {
 // console.log(mainCarousel);
