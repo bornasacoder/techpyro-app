4bc77fdc3fb550ae80138a0d92ff12f487ffa53a
= <=>
2023-06-07 16:02:20 +0530

    comiit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Home from "pages/home/Home";
+import Home from "./pages/home/Home";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Card3 from "components/card/card2/Card3";
-import Card4 from "components/card/Card4";
-import Card5 from "components/card/Card5";
-import Login from "components/card/card2/Login";
-import Aboutus from "components/card/card2/Aboutus";
+import Card3 from "./components/card/card2/Card3";
+import Card4 from "./components/card/Card4";
+import Card5 from "./components/card/Card5";
+import Login from "./components/card/card2/Login";
+import Aboutus from "./components/card/card2/Aboutus";
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
   
         <Route path='/' element={  <Home />}/>
         <Route path='/Admission Form' element={ <Card3 />}/>
-        <Route path="/Contact Us" element={<Card4 />}/>
+        <Route path="/Contact Us./" element={<Card4 />}/>
         <Route path="/Search" element={<Card5 />}/>
         <Route path="Login" element={<Login />}/>
         <Route path="About Us" element={<Aboutus />}/>
