8d8758a7fa42b7cae64120a7c3f06b9fddaac4ea
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-06-07 17:18:31 +0530

    nitish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,15 @@
-// import MainCarousel from 'components/carousel/MainCarousel'
-import Sliders from 'components/slider/slider1/Sliders'
+
+import Sliders from '../../components/slider/slider1/Sliders'
 import React from 'react'
-import Navbar from 'components/navbar/Navbar';
-import MiddleContent from 'components/middlecontent/MiddleContent';
-import Footer from 'components/footer/Footer';
-import Content1 from 'components/content/Content1';
-import Content2 from 'components/content/Content2';
-import Content4 from 'components/content/Content4';
-import Content5 from 'components/content/Content5';
-import Content6 from 'components/content/Content6';
-import Content7 from 'components/content/Content7';
+import Navbar from '../../components/navbar/Navbar';
+import MiddleContent from '../../components/middlecontent/MiddleContent';
+import Footer from '../../components/footer/Footer';
+import Content1 from '../../components/content/Content1';
+import Content2 from '../../components/content/Content2';
+import Content4 from '../../components/content/Content4';
+import Content5 from '../../components/content/Content5';
+import Content6 from '../../components/content/Content6';
+import Content7 from '../../components/content/Content7';
 
 
 const Home = () => {
