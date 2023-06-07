1fa52c4ed709faa82d1218ebddee1520a30ab57f
Nitish847235 <nitishpatwarabihar@gmail.com>
2023-06-07 18:29:13 +0530

    nitish

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ThemeProviderWrapper from 'theme/ThemeProvider';
 import { CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
