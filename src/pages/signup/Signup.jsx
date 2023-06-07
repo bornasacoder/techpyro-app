5533f5359aad3f5a38bc257793d1f3f0f92b4857
= <=>
2023-06-07 21:06:02 +0530

    comiit

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import { Typography } from '@mui/material'
-import Block1 from 'contents/signup/Block1'
-import Navbar from 'layouts/Navbar/Navbar'
+import Block1 from '../../contents/signup/Block1'
+import Navbar from '../../layouts/Navbar/Navbar'
 import React from 'react'
 
 const Signup = () => {
