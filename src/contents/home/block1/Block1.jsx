51bbed5ded7db24fcf2dc5a751cfbb840ebdbb5d
harsh7268 <harshpatel726891@gmail.com>
2023-06-17 18:58:48 +0530

    update

--- src/contents/home/block1/Block1.jsx
+++ src/contents/home/block1/Block1.jsx
@@ -5,22 +5,22 @@ import Item from './Item';
 
 export default function Block1() {
   const data = [
-    {img:'https://img.freepik.com/free-photo/couple-looking-shop-window_1098-758.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=ais',name:'Burger'},
-    {img:'https://img.freepik.com/free-photo/young-couple-protection-mask-holding-multiple-paper-shopping-bag-walking-corridor-large-shopping-mall_1150-47177.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=ais',name:'Coffee'},
-    {img:'https://img.freepik.com/free-photo/this-is-same-shoes_329181-1769.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=ais',name:'Coke'},
-    {img:'https://img.freepik.com/free-photo/market_1205-448.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Chips'},
-    {img:'https://img.freepik.com/free-photo/shoppers-talking-center_1098-12640.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Mix Food'},
-    {img:'https://img.freepik.com/free-photo/weekend-shopping-center_1098-12634.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/two-beautiful-women-shopping-town_1303-16426.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Burger'},
-    {img:'https://img.freepik.com/free-vector/customers-fashion-shop-women-choosing-clothes-store-flat-vector-illustration-shopping-sale-retail-concept_74855-9836.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/mother-daughter-with-shopping-bag_1157-21280.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/couple-lover-activity-happiness-lifestyle_53876-13941.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/family-mall_1098-15029.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/female-friends-out-shopping-together_53876-25041.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-vector/advertising-flyer-two-mothers-with-prams-cartoon_81522-4317.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-vector/people-returning-malls-new-normal-scenes_52683-41108.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/mother-daughter-posing-near-boutique-with-shopping-bags_7502-4268.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Burger'},
-    {img:'https://img.freepik.com/free-photo/cheery-little-girl-sitting-shopping-cart_171337-2374.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'French Fries'},
+    {img:'https://img.freepik.com/free-photo/pair-gold-earrings-with-green-stones-black-background_1340-42887.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-photo/gold-necklace-with-necklace-earrings_1340-42825.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coffee'},
+    {img:'https://img.freepik.com/free-photo/shiny-gemstone-necklace-reflects-elegance-glamour-generated-by-ai_188544-10583.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coke'},
+    {img:'https://img.freepik.com/free-photo/gold-necklace-with-necklace-earrings_1340-42825.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Chips'},
+    {img:'https://img.freepik.com/free-photo/shiny-gemstone-necklace-reflects-elegance-glamour-generated-by-ai_188544-10583.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Mix Food'},
+    {img:'https://img.freepik.com/free-photo/shiny-gold-wedding-ring-with-diamond-gemstone-generated-by-ai_188544-37378.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/shiny-gold-jewelry-symbol-wealth-generated-by-ai_188544-10733.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-vector/horizontal-jewelry-realistic-banners-with-golden-rings-elegant-necklaces-mannequins_1284-16774.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-vector/horizontal-jewelry-realistic-banners-with-golden-rings-elegant-necklaces-mannequins_1284-16774.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/selective-focus-closeup-diamond-rings_181624-57691.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/shiny-gemstone-necklace-reflects-elegance-glamour-generated-by-ai_188544-13941.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/wedding-preparation-beautiful-happy-bride-dresses-earrings-before-wedding_8353-5813.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/beautiful-luxury-necklace-jewelry-stand-neck_1339-7947.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/closeup-diamond-ring_53876-17957.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-psd/elegant-wedding-banner-template_23-2148945298.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-photo/selective-focus-closeup-diamond-rings_181624-57691.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'French Fries'},
     ]
   return (
       <Box sx={{margin:{xs:'140px 20px 40px',md:'120px 20px 40px'},display:'flex',flexDirection:'column'}}>
--- src/layouts/layout1/navbar/Navbar.jsx
+++ src/layouts/layout1/navbar/Navbar.jsx
@@ -178,7 +178,7 @@ export default function Navbar() {
             <LocalPizzaIcon/>
             </MenuButtons>
           <Typography variant="h1" fontSize="20px">
-          Mall
+           Jwellery
           </Typography>
           </Box>
            <Drawer open={open} onClose={handleClose} sx={{ position: "absolute", }}>
@@ -232,9 +232,9 @@ export default function Navbar() {
     </NavBarTop>
 
     <NavBarBottom>
-      <Typography sx={{fontSize:'16px',fontWeight:'600',background:'#DD3739',color:'#ff',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Grocery</Typography>
-      <Typography sx={{color:'black',fontSize:'16px',fontWeight:'600',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',borderLeft:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Clothes</Typography>
-      <Typography sx={{color:'black',fontSize:'16px',fontWeight:'600',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',borderLeft:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Footwear</Typography> 
+      <Typography sx={{fontSize:'16px',fontWeight:'600',background:'#DD3739',color:'#ff',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Ring</Typography>
+      <Typography sx={{color:'black',fontSize:'16px',fontWeight:'600',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',borderLeft:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Necklace</Typography>
+      <Typography sx={{color:'black',fontSize:'16px',fontWeight:'600',border:'.1px solid rgba(0,0,0,0.3)',borderTop:'0',borderBottom:'0',borderLeft:'0',padding:{xs:'8px 10px',md:'15px 20px'}}}>Diamond</Typography> 
 
     </NavBarBottom>
 
