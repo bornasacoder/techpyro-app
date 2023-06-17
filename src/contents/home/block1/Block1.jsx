016d74c4815a2fa12b82f6975173c067a80c949f
harsh7268 <harshpatel726891@gmail.com>
2023-06-17 17:51:00 +0530

    update

--- src/contents/home/block1/Block1.jsx
+++ src/contents/home/block1/Block1.jsx
@@ -5,22 +5,22 @@ import Item from './Item';
 
 export default function Block1() {
   const data = [
-    {img:'https://img.freepik.com/free-photo/television-houseplants-room-scene-generative-ai_188544-12145.jpg?size=626&ext=jpg&ga=GA1.2.833918949.1682592323&semt=sph',name:'Burger'},
-    {img:'https://img.freepik.com/free-vector/tv-screen-wall-with-neon-light_134830-819.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coffee'},
-    {img:'https://img.freepik.com/free-photo/young-woman-wearing-headphones-sitting-sofa-coffee-table-living-room_141793-122743.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coke'},
-    {img:'https://img.freepik.com/free-vector/air-conditioner-with-flows-cold-air-wall-realistic-vector-illustration_1284-76980.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Chips'},
-    {img:'https://img.freepik.com/free-vector/modern-washing-machine-empty-laundry-room_1284-33056.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Mix Food'},
-    {img:'https://img.freepik.com/free-vector/washing-machine-realistic-set_1284-29777.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-vector/3d-realistic-mock-up-with-gray-two-chambered-refrigerator-isolated-tile-wall_1441-2146.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
-    {img:'https://img.freepik.com/free-vector/refrigerator-organization-set_1284-23312.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-psd/tablet-mock-up-isolated_1310-1643.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=ais',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/businessman-relaxing-office_1098-21722.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/full-shot-smiley-woman-sitting-chair_23-2149410594.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-vector/different-heaters-interior-realistic-illustration_1284-59763.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-photo/full-shot-blurry-couple-sitting-couch_23-2149308945.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-vector/modern-desktop-computer-concept-illustration_114360-16235.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
-    {img:'https://img.freepik.com/free-psd/laptop-black-background-mock-up-design_1135-80.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
-    {img:'https://img.freepik.com/free-vector/realistic-display-smartphone-with-different-apps_52683-30241.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'French Fries'},
+    {img:'https://img.freepik.com/free-photo/spring-wardrobe-switch-still-life_23-2150176679.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-photo/women-heeled-shoes_1303-5875.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coffee'},
+    {img:'https://img.freepik.com/free-psd/white-shoes-mockup_1310-647.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Coke'},
+    {img:'https://img.freepik.com/free-vector/pair-leather-sports-shoes-wood-background_1284-17526.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Chips'},
+    {img:'https://img.freepik.com/free-photo/high-heel-shoes_1203-8674.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Mix Food'},
+    {img:'https://img.freepik.com/free-photo/still-life-say-no-fast-fashion_23-2149669605.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/fashion-shoes_1203-7555.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-photo/brown-flip-flops-summer-footwear-fashion_53876-106037.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/leather-shoes-wooden-background_1203-7618.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-vector/sale-banner-with-product-description_1361-1333.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/woman-posing-with-stylish-footwear-summer-fashion-bag-long-legs-shopping_285396-489.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/boots-wooden-floor_1205-70.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/brown-woman-shoe-with-female-male-hands_155003-33390.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/young-woman-shopping-with-high-heels-shopping-center_1149-1969.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Marita'},
+    {img:'https://img.freepik.com/free-photo/woman-shoes-isolated_1203-8368.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'Burger'},
+    {img:'https://img.freepik.com/free-psd/new-collection-fashion-sale-web-banner-template_120329-1507.jpg?size=626&ext=jpg&ga=GA1.1.833918949.1682592323&semt=sph',name:'French Fries'},
     ]
   return (
       <Box sx={{margin:{xs:'140px 20px 40px',md:'120px 20px 40px'},display:'flex',flexDirection:'column'}}>
